b159969cd20bbdb7b221d61e901a87031d7bbc50
Szymon Chamera <szymon.chamera@icloud.com>
2023-12-27 11:52:56 +0100

    Pierwsza wersja funkcji eliminate w pliku gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -4,11 +4,29 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+int eliminate(Matrix *mat, Matrix *b)
+{
+	int i, j, k;
+	int n = mat->r;
 
-		return 0;
+	for (i = 0; i < n; i++)
+	{
+
+		for (j = i + 1; j < n; j++)
+		{
+			double factor = mat->data[j][i] / mat->data[i][i];
+			for (k = i; k < n; k++)
+			{
+				mat->data[j][k] -= factor * mat->data[i][k];
+			}
+			b->data[j][0] -= factor * b->data[i][0];
+		}
+
+		if (mat->data[i][i] == 0.0)
+		{
+			return 1;
+		}
+	}
+
+	return 0;
 }
-
